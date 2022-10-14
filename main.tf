41536c1d26b5488bd626c9533a1e7389871d220d
jackson- <jackson.t.devin@gmail.com>
2022-10-14 16:41:52 -0400

    init commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("terraform-gcp-tutorial-365520-348915d79071.json")
+
+  project = "terraform-gcp-tutorial-365520"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
